3e7e49a98cd7f6ddb3198f2cd4df60af183ca8a2
besin012 <wldbs6988@naver.com>
2025-05-29 16:00:06 +0900

    자기소개

---
+++ loadiog.js
@@ -0,0 +1,8 @@
+// laoding 
+$(window).load(function(){
+    $('.loading_container').delay(2000).fadeOut();
+})
+
+window.addEventListener('load', function () {
+  document.querySelector('.fixed-menu').style.display = 'flex';
+});
